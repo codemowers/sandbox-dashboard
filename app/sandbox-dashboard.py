ebb34fbb7219183a762bbd2b3ce0f34591acaae8
Lauri Võsandi <lauri@k-space.ee>
2022-11-24 19:22:01 +0200

    Make more stuff configurable

--- app/sandbox-dashboard.py
+++ app/sandbox-dashboard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import random
-import sys
 import string
 import yaml
 from functools import wraps
@@ -12,13 +12,25 @@ from kubernetes_asyncio import client, config
 from sanic_wtf import SanicForm
 from wtforms import BooleanField
 
+parser = argparse.ArgumentParser(description="Run Kubernetes cluster sandbox dashboard")
+parser.add_argument("--cluster-name",
+    default="codemowers.eu")
+parser.add_argument("--cluster-api-url",
+    default="https://kube.codemowers.eu")
+parser.add_argument("--context-name-prefix",
+    default="codemowers.eu/")
+parser.add_argument("--sandbox-template-url",
+    default="git@git.k-space.ee:codemowers/lab-template")
+parser.add_argument("--feature-flags-spec",
+    default="/config/playground.yml")
+args = parser.parse_args()
+
 ANNOTATION_MANAGED_BY = "sandbox-dashboard"
 
 fallback_email = "lauri.vosandi@gmail.com"
 characters = "abcdefghijkmnpqrstuvwxyz23456789"
 app = Sanic("dashboard")
 session = {}
-_, path = sys.argv
 
 if not app.config.get("WTF_CSRF_SECRET_KEY", ""):
     raise ValueError("SANIC_WTF_CSRF_SECRET_KEY environment variable not set")
@@ -28,9 +40,11 @@ class PlaygroundForm(SanicForm):
     pass
 
 
-with open(path) as fh:
+with open(args.feature_flags_spec) as fh:
     feature_flags = yaml.safe_load(fh.read())
     for feature_flag in feature_flags:
+        if feature_flag.get("disabled"):
+            continue
         kwargs = {
             "name": feature_flag["name"],
             "description": feature_flag["description"],
@@ -114,7 +128,7 @@ async def create_sandbox(user, values):
         "spec": {
             "project": "default",
             "source": {
-                "repoURL": "git@git.k-space.ee:codemowers/lab-template",
+                "repoURL": args.sandbox_template_url,
                 "path": "./",
                 "targetRevision": "HEAD",
                 "helm": {
@@ -194,6 +208,7 @@ async def sandbox_detail(request, sandbox_name):
         network_api = client.NetworkingV1Api(api)
         argo_app = await api_instance.get_namespaced_custom_object("argoproj.io", "v1alpha1", "argocd", "applications", sandbox_name)
         return {
+            "args": args,
             "namespace": sandbox_name,
             "sandbox": wrap_sandbox_parameters(argo_app),
             "pods": (await v1.list_namespaced_pod(sandbox_name)).items,
@@ -218,6 +233,7 @@ async def handler(request):
             if not email or w["parameters"].get("email") == email:
                 sandboxes.append(w)
         return {
+            "args": args,
             "feature_flags": feature_flags,
             "sandboxes": sandboxes
         }
